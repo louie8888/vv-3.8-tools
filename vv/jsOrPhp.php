da716171fe5f5b049456b500cd65ab718afa654f
louie8888 <louie8888@yahoo.com>
2021-07-19 02:54:47 +0800

    新增调用方式开关，切换js或者是php调用方式

---
+++ vv/jsOrPhp.php
@@ -0,0 +1,12 @@
+<?php
+$js = 0;//代表不用js跳转，用php
+//$js = 1;//代表用js跳转，不用php
+/*header('Content-Type:application/json; charset=utf-8');
+$arr = array('js'=>$js);
+
+exit(json_encode($arr));*/
+/*header('Content-Type:application/json; charset=utf-8');
+$arr = array('js'=>$js);
+
+exit(json_encode($arr));*/
+echo $js;
