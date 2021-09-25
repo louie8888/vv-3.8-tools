9c355ace65de4dea0653ad926c35c2e4e76bd6a6
louie8888 <louie8888@yahoo.com>
2021-09-26 00:04:25 +0800

    修改无广告统计器

--- vv/static.js
+++ vv/static.js
@@ -1,8 +1,11 @@
-//调用无广告的百度统计器js替代广告js 
+//无广告百度统计器
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?549069c2c2b87e31e3cf080b63a96bb8";
+  hm.src = "https://hm.baidu.com/hm.js?ab521c9794f77cae332121b394612962";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
+//无广告51la
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JRwjZLuzpoFBDZFc\",ck:\"JRwjZLuzpoFBDZFc\"});</script>")
